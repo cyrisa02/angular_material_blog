899d95bafdff7a4c86e3b2d43965666af465c524
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-10-31 14:49:26 +0100

    Add Login

---
+++ readme.txt
@@ -0,0 +1,32 @@
+dans app.module 
+import { MaterialDesignModule } from './material-design/material-design.module'; 
++ imports -> voir fichier
+
+-----------------------------------------
+
+pour créer un module globale
+ng generate module MaterialDesign
+ 26 min
+aller chercher les modules qui m'intéressent dans https://material.angular.io/components/card/api et faire l'exports dans le material-design.module
+
+----------------
+navbar s'appelle toolbar 30min
+
+---------------
+Modifier le fichier général du css 
+.flex {
+    display: flex;
+}
+
+.justify-content-between{
+    justify-content: space-between;
+}
+-----------------------------
+Création de la route login
+
+ng generate component auth/login
+Modifier le app-routing.module
++ <router-outlet></router-outlet> dans app.component.html 
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesignModule } from './material-design/material-design.module';
+import { LoginComponent } from './auth/login/login.component';
+import { BlogPageComponent } from './blog/blog-page/blog-page.component';
+import { SingleBlogComponent } from './blog/single-blog/single-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BlogPageComponent,
+    SingleBlogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialDesignModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
